ec5fe4d905ec65310a207714da8adbdc379c1ec5
Felipe Pucci <traumerei_@hotmail.com>
2018-04-15 12:07:27 -0300

    Add files via upload

---
+++ HomeMade/Doação de sangue.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ int main()
+ {
+ 	
+     int idade;
+
+     printf("Digite sua idade\n");
+     scanf("%d", &idade);
+     
+     if (idade > 67 || idade < 18)
+     {
+        printf("Voce nao pode doar sangue\n");
+	 }
+	    else
+		{
+		printf("Abre esse braco ai, vamos sugar essa groselha e fazer um churisso pro pessoal\n");
+	}
+	    
+}
